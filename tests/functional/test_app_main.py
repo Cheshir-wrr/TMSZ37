e66e20e056cf24cbc0a0486e39682eacb689e7e8
HP <macuo_base@mail.ru>
2020-10-20 20:38:24 +0300

    VotTak

--- tests/functional/test_app_main.py
+++ tests/functional/test_app_main.py
@@ -23,7 +23,7 @@ def validate_content(page: MainPage):
     assert page.h1.tag_name == "h1"
     assert page.h1.text == "Hey There"
     assert page.p.tag_name == "p"
-    assert page.p.text == "This is a study project."
+    assert page.p.text == "I Want to Break Free!"
 
     html = page.html
     assert "<hr>" in html
