7622d62808e69fae0601c3ac4b006ca79616c124
Cheshir-wrr <72446867+Cheshir-wrr@users.noreply.github.com>
2020-10-23 20:49:34 +0300

    VotTak (#4)
    * VotTak
    
    * VotTak
    
    * VotTak
    
    * VotTak
    
    * VotTak
    
    * VotTak
    
    * Словарь

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,45 +1,27 @@
+import mimetypes
+
 from framework.consts import DIR_STATIC
 
 
 def application(environ, start_response):
     url = environ["PATH_INFO"]
-    if url == "/xxx/":
-        status = "200 OK"
-        headers = {
-            "Content-type": "text/css",
-        }
-        payload = read_from_styles_css()
-        start_response(status, list(headers.items()))
+    file_names = {"/xxx/": "style.css", "/img.jpg/": "img.jpg"}
+    file_name = file_names.get(url, "index.html")
+    status = "200 OK"
 
-        yield payload
-    else:
-        status = "200 OK"
-        headers = {
-            "Content-type": "text/html",
-        }
+    headers = {
+        "Content-type": mimetypes.guess_type(file_name)[0],
+    }
+    payload = read_static(file_name)
+    start_response(status, list(headers.items()))
 
-        payload = read_from_index_html()
-
-        start_response(status, list(headers.items()))
-
-        yield payload
+    yield payload
 
 
-def read_from_index_html():
-    path = DIR_STATIC / "index.html"
+def read_static(file_name: str) -> bytes:
+    path = DIR_STATIC / file_name
 
-    with path.open("r") as fp:
+    with path.open("rb") as fp:
         payload = fp.read()
 
-    payload = payload.encode()
-    return payload
-
-
-def read_from_styles_css():
-    path = DIR_STATIC / "style.css"
-
-    with path.open("r") as fp:
-        payload = fp.read()
-
-    payload = payload.encode()
     return payload
