3592cdee375a9b81d4f56688189e92d1a035f3d2
HP <macuo_base@mail.ru>
2020-10-23 20:44:58 +0300

    Словарь

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,63 +1,25 @@
+import mimetypes
+
 from framework.consts import DIR_STATIC
 
 
 def application(environ, start_response):
     url = environ["PATH_INFO"]
-    if url == "/xxx/":
-        status = "200 OK"
-        headers = {
-            "Content-type": "text/css",
-        }
-        payload = read_from_styles_css()
-        start_response(status, list(headers.items()))
+    file_names = {"/xxx/": "style.css", "/img.jpg/": "img.jpg"}
+    file_name = file_names.get(url, "index.html")
+    status = "200 OK"
 
-        yield payload
+    headers = {
+        "Content-type": mimetypes.guess_type(file_name)[0],
+    }
+    payload = read_static(file_name)
+    start_response(status, list(headers.items()))
 
-    elif url == "/img.jpg/":
-        status = "200 OK"
-        headers = {
-            "Content-type": "image/jpeg",
-        }
-        payload = read_from_logo()
-        start_response(status, list(headers.items()))
-
-        yield payload
-
-    else:
-        status = "200 OK"
-        headers = {
-            "Content-type": "text/html",
-        }
-
-        payload = read_from_index_html()
-
-        start_response(status, list(headers.items()))
-
-        yield payload
+    yield payload
 
 
-def read_from_index_html():
-    path = DIR_STATIC / "index.html"
-
-    with path.open("r") as fp:
-        payload = fp.read()
-
-    payload = payload.encode()
-    return payload
-
-
-def read_from_styles_css():
-    path = DIR_STATIC / "style.css"
-
-    with path.open("r") as fp:
-        payload = fp.read()
-
-    payload = payload.encode()
-    return payload
-
-
-def read_from_logo():
-    path = DIR_STATIC / "img.jpg"
+def read_static(file_name: str) -> bytes:
+    path = DIR_STATIC / file_name
 
     with path.open("rb") as fp:
         payload = fp.read()
