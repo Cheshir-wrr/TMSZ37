3adb321697e07b21be5678773d19666e40f1e992
Cheshir-wrr <72446867+Cheshir-wrr@users.noreply.github.com>
2020-11-03 23:14:33 +0300

    server_error+hello (#8)

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,29 +1,45 @@
+from urllib.parse import parse_qs
+
 from framework.types import RequestT
+from handlers.error import make_error
 from handlers.handle_image import handle_image
 from handlers.handle_index import handle_index
+from handlers.hello import hello
 from handlers.not_found import generate_404
+from handlers.server_error import generate_500
 from handlers.Styles import handle_style
 
-handlers = {"/xxx/": handle_style, "/img.jpg/": handle_image, "/": handle_index}
+handlers = {
+    "/xxx/": handle_style,
+    "/img.jpg/": handle_image,
+    "/": handle_index,
+    "/e/": make_error,
+    "/name/": hello,
+}
 
 
 def application(environ: dict, start_response):
-    path = environ["PATH_INFO"]
+    try:
+        path = environ["PATH_INFO"]
 
-    handler = handlers.get(path, generate_404)
+        handler = handlers.get(path, generate_404)
 
-    request_headers = {
-        key[5:]: environ[key]
-        for key in filter(lambda i: i.startswith("HTTP_"), environ)
-    }
+        request_headers = {
+            key[5:]: environ[key]
+            for key in filter(lambda i: i.startswith("HTTP_"), environ)
+        }
 
-    request = RequestT(
-        method=environ["REQUEST_METHOD"],
-        path=path,
-        headers=request_headers,
-    )
+        request = RequestT(
+            method=environ["REQUEST_METHOD"],
+            path=path,
+            headers=request_headers,
+            query=parse_qs(environ.get("QUERY_STRING") or ""),
+        )
 
-    response = handler(request)
+        response = handler(request)
+
+    except Exception:
+        response = generate_500(request)
 
     start_response(response.status, list(response.headers.items()))
 
