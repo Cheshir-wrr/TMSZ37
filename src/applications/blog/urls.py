65b1a42e34c5ca599a2b733600ca1248baba324b
HP <macuo_base@mail.ru>
2020-12-04 21:56:32 +0300

    переход на generic

--- src/applications/blog/urls.py
+++ src/applications/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from applications.blog import views
 
-from applications.blog.views import blog_reset
-from applications.blog.views import index
-from applications.blog.views import new_post_view
 
-urlpatterns = [path("", index), path("new/", new_post_view), path("reset/", blog_reset)]
+urlpatterns = [path("", views.AllPostsView.as_view()),
+               path("new/", views.NewPostView.as_view()),
+               ]
