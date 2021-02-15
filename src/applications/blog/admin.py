0550be4533af2492ee2db5f303e914e03b7977a6
Cheshir-wrr <72446867+Cheshir-wrr@users.noreply.github.com>
2021-02-15 21:46:27 +0200

    комменты+поиск постов (#24)

--- src/applications/blog/admin.py
+++ src/applications/blog/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
+from applications.blog.models import Comments
 from applications.blog.models import Post
 
 
 @admin.register(Post)
 class PostAdminModel(admin.ModelAdmin):
     pass
+
+
+@admin.register(Comments)
+class CommentAdminModel(admin.ModelAdmin):
+    pass
+
+
+# class Comment(admin.ModelAdmin):
+#     list_display = ('name', 'email', 'post', 'created', 'active')
+#     list_filter = ('active', 'created', 'updated')
+#     search_fields = ('name', 'email', 'body')
+#
+#
+# admin.site.register(Comment, Comment)
